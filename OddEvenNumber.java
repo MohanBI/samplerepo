e416145188552ef2fc9fb14c26bf0bc0e1ce6ae3
mohan <mohan@.com>
2020-05-26 20:14:35 +0530

    odd or even number script

---
+++ OddEvenNumber.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class OddEvenNumber {
+
+	public static void main(String[] args) {
+		
+		int number=26;
+		
+		
+				if(number%2==0)
+		{
+			System.out.println("The given number is Even Number");
+		}
+		else
+		{
+			System.out.println("The given number is odd");
+		}
+		
+
+	}
+
+	
+}
